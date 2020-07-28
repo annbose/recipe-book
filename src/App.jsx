47ea6e32ff0149a5f9f1ba49928bb4981c42caf4
annbose <ann.bose@outlook.com>
2020-07-28 16:47:36 +1000

    added react slick

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 import Carousel from "./carousel.jsx"
+import Navbar from './navbar';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
       <div className="bg">
         <h1 >Cook with love</h1>
       </div>
-
-     {/* <Carousel 
-    //  
-    /> */}
+    
+     <Carousel 
+    
+    />
     
     </div>
   );
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import "./navbar.css";
 
 function Navbar(){
-    return <div className="navbar-parent">
-        <span>Ann's Recipes</span>
-        <input></input>
 
-        <categories></categories>
+    function handleChange(event){
+        console.log(event.target.value)
+    }
+
+    function handleSearchClick(){
+        console.log("i gt clicked")
+
+    }
+
+
+    return <div className="navbar-parent">
+        <span className="nav-title">Ann's Recipes</span>
+        
+        <input className="nav-input"
+        onChange={handleChange} ></input>
+
+        <i className="nav-i"
+        onClick={handleSearchClick}class="fas fa-search"></i>
+        
+
+        <span className="nav-cat">Categories</span>
+        <i class="fas fa-caret-down"></i>
 
 
     </div>
